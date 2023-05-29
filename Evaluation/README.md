ef67c264cce7f58037c1f0f2c5ed01b868b34cbc
Yutian Chen <markchenyutian@gmail.com>
2023-05-29 14:21:13 -0400

    Resolve PR review requests, correct README.md

--- Evaluation/README.md
+++ Evaluation/README.md
@@ -1,10 +1,14 @@
 ## Trajectory Format
 
-The output trajectory's format is the same as TUM trajectory format under NED coordinate, with 8 columns, each representing
+The output trajectory's format is the same as TUM trajectory format under EDN coordinate, with 8 columns, each representing
 
 ```
 timestamp tx ty tz qx qy qz qw
 ```
+
+## Environment
+
+`evo` ([Link to pypi](https://pypi.org/project/evo/)) and `matplotlib` are required for this evaluation script.
 
 ## Run Evaluation
 
--- Evaluation/evaluate.py
+++ Evaluation/evaluate.py
@@ -9,35 +9,37 @@ from pathlib import Path
 import argparse
 import matplotlib.pyplot as plt
 
-Parser = argparse.ArgumentParser(description="Plot and evaluate the AirDOS output trajectory on TartanAir-shibuya Dataset")
-Parser.add_argument("--estimate", action="store", required=True)
-Parser.add_argument("--gt", action="store", required=True)
-Parser.add_argument("--real_perspective", action="store_true", default=False)
+if __name__ == "__main__":
+    Parser = argparse.ArgumentParser(description="Plot and evaluate the AirDOS output trajectory on TartanAir-shibuya Dataset")
+    Parser.add_argument("--estimate", action="store", required=True, help="Estimated trajectory file (*.txt) with 8 columns")
+    Parser.add_argument("--gt", action="store", required=True, help="Ground truth trajectory file (*.txt) in TartanAir-shibuya dataset")
+    Parser.add_argument("--real_perspective", action="store_true", default=False, help="Add this option to enforce 1:1 perspective ratio in plotting")
 
 
-args = Parser.parse_args()
-est_traj_path = Path(args.estimate)
-gt_traj_path = Path(args.gt)
+    args = Parser.parse_args()
+    print(args)
+    est_traj_path = Path(args.estimate)
+    gt_traj_path = Path(args.gt)
 
-assert est_traj_path.exists()
-assert gt_traj_path.exists()
+    assert est_traj_path.exists()
+    assert gt_traj_path.exists()
 
-SETTINGS.plot_xyz_realistic = args.real_perspective
-est_traj = read_tum_trajectory_file(est_traj_path)
-gt_traj = read_tum_trajectory_file(gt_traj_path)
+    SETTINGS.plot_xyz_realistic = args.real_perspective
+    est_traj = read_tum_trajectory_file(est_traj_path)
+    gt_traj = read_tum_trajectory_file(gt_traj_path)
 
-ape_output = ape(gt_traj, est_traj, pose_relation=PoseRelation.translation_part, align=True, correct_scale=False)
-rpe_t_output = rpe(gt_traj, est_traj, pose_relation=PoseRelation.translation_part, delta=1.0, delta_unit=Unit.frames,
-                 all_pairs=True, align=True, correct_scale=False)
-rpe_r_output = rpe(gt_traj, est_traj, pose_relation=PoseRelation.rotation_part, delta=1.0, delta_unit=Unit.frames,
-                 all_pairs=True, align=True, correct_scale=False)
+    ape_output = ape(gt_traj, est_traj, pose_relation=PoseRelation.translation_part, align=True, correct_scale=False)
+    rpe_t_output = rpe(gt_traj, est_traj, pose_relation=PoseRelation.translation_part, delta=1.0, delta_unit=Unit.frames,
+                    all_pairs=True, align=True, correct_scale=False)
+    rpe_r_output = rpe(gt_traj, est_traj, pose_relation=PoseRelation.rotation_part, delta=1.0, delta_unit=Unit.frames,
+                    all_pairs=True, align=True, correct_scale=False)
 
-print("ATE:", round(ape_output.stats["rmse"], 3))
-print("RPE (Translation)", round(rpe_t_output.stats["rmse"], 3))
-print("RPE (Rotation)", round(rpe_r_output.stats["rmse"], 3))
+    print("ATE:", round(ape_output.stats["rmse"], 3))
+    print("RPE (Translation)", round(rpe_t_output.stats["rmse"], 3))
+    print("RPE (Rotation)", round(rpe_r_output.stats["rmse"], 3))
 
-fig = plt.figure(dpi=200)
-ax = fig.add_subplot(1, 1, 1)
-traj(ax, plot_mode=PlotMode.xz, traj=est_traj, color="r", label="AirDOS")
-traj(ax, plot_mode=PlotMode.xz, traj=gt_traj, color="b", label="Ground Truth")
-plt.show()
+    fig = plt.figure(dpi=200)
+    ax = fig.add_subplot(1, 1, 1)
+    traj(ax, plot_mode=PlotMode.xz, traj=est_traj, color="r", label="AirDOS")
+    traj(ax, plot_mode=PlotMode.xz, traj=gt_traj, color="b", label="Ground Truth")
+    plt.show()
